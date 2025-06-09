4292e3b3ec2a914e0171c40009a39682e784a176
jvbaracho <joao.lira@novaroma.edu.br>
2025-06-09 16:50:08 -0300

    Delete fullstack directory

--- fullstack/backend/src/config/validators/customerValidator.js
+++
@@ -1,6 +0,0 @@
-const { body } = require('express-validator');
-
-exports.validateCustomer = [
-  body('name').notEmpty(),
-  body('address').notEmpty(),
-];
--- fullstack/backend/src/config/validators/userValidator.js
+++
@@ -1,7 +0,0 @@
-const { body } = require('express-validator');
-
-exports.validateUpdateUser = [
-  body('name').optional().notEmpty(),
-  body('email').optional().isEmail(),
-  body('role').optional().isIn(['user', 'admin']),
-];
