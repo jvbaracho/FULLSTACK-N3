655968122ddafd1dad942d95c04cad1905cc7809
jvbaracho <joao.lira@novaroma.edu.br>
2025-06-09 20:26:56 -0300

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,3 +1,3 @@
 PORT=5000
-MONGO_URI=mongodb+srv://<jvbaracho2004>:<hxzza;gdkD7#=Ra>@cluster.mongodb.net/pizzaria
+MONGO_URI=mongodb+srv://<db_username>:<db_password>@pizzariaclustern3.fus6jak.mongodb.net/?retryWrites=true&w=majority&appName=PIZZARIACLUSTERN3
 JWT_SECRET=sua_chave_secreta_supersegura
