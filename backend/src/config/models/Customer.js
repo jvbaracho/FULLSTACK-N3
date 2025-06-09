f022567848169f08357317f90d27d0c2085af3cc
jvbaracho <joao.lira@novaroma.edu.br>
2025-06-09 16:55:32 -0300

    Delete backend directory

--- backend/src/config/models/Customer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const customerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  phone: { type: String },
-  address: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Customer', customerSchema);
--- backend/src/config/validators/orderValidator.js
+++
@@ -1,9 +0,0 @@
-const { body } = require('express-validator');
-
-exports.validateOrder = [
-  body('customer').notEmpty(),
-  body('products').isArray({ min: 1 }),
-  body('products.*.product').notEmpty(),
-  body('products.*.quantity').isInt({ gt: 0 }),
-  body('total').isFloat({ gt: 0 }),
-];
