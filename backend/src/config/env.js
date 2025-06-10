cf00f0a8a82f8aed55cc7a24e169c34a6fd2d1a2
jvbaracho <joao.lira@novaroma.edu.br>
2025-06-10 20:08:28 -0300

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,3 +1,3 @@
 PORT=5000
-MONGO_URI=mongodb+srv://admin:admin123@cluster0.abcd1.mongodb.net/pizzaria?retryWrites=true&w=majority
+MONGO_URI=mongodb+srv://<ADMIN>:<admin123>@pizzariaclustern3.fus6jak.mongodb.net/pizzaria?retryWrites=true&w=majority&appName=PIZZARIACLUSTERN3
 JWT_SECRET=sua_chave_secreta
