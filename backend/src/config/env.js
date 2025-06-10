bbaa5ad156722f6b0cd71fe2d3ae95dba31305f3
jvbaracho <joao.lira@novaroma.edu.br>
2025-06-10 20:03:20 -0300

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,30 +1,3 @@
 PORT=5000
-MONGO_URI=
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<jvbaracho2004>:<hxzza;gdkD7#=Ra>@cluster-shard-00-00.mongodb.net:27017,cluster-shard-00-01.mongodb.net:27017,cluster-shard-00-02.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-xxxxx-shard-0&authSource=admin&retryWrites=true&w=majority
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-JWT_SECRET=sua_chave_secreta_supersegura
+MONGO_URI=mongodb+srv://admin:admin123@cluster0.abcd1.mongodb.net/pizzaria?retryWrites=true&w=majority
+JWT_SECRET=sua_chave_secreta
