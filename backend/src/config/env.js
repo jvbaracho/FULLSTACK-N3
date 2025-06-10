cb673ce0fa2184f48c63f9d68a22737274347be9
jvbaracho <joao.lira@novaroma.edu.br>
2025-06-10 18:22:05 -0300

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,7 +1,8 @@
 PORT=5000
 MONGO_URI=
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<jvbaracho2004>:<hxzza;gdkD7#=Ra>@pizzariaclustern3.fus6jak.mongodb.net/?retryWrites=true&w=majority&appName=PIZZARIACLUSTERN3";
+const uri = "mongodb+srv://<jvbaracho2004>:<hxzza;gdkD7#=Ra>@cluster-shard-00-00.mongodb.net:27017,cluster-shard-00-01.mongodb.net:27017,cluster-shard-00-02.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-xxxxx-shard-0&authSource=admin&retryWrites=true&w=majority
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
